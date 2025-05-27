d9f2cae064c36422a8a3bf1e28a2164e0466401d
Lakub <kuba.loszewski@op.pl>
2025-05-27 15:22:16 +0200

    Rozpoczęcie data editor

---
+++ Nauron/DataEditor.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Nauron
+{
+    /// <summary>
+    /// Logika interakcji dla klasy DataEditor.xaml
+    /// </summary>
+    public partial class DataEditor : Window
+    {
+        public DataEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
