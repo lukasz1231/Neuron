05eeb34b468d53684cfb9272bc00e394deab046c
lkuci <lkucikowicz@gmail.com>
2025-05-30 16:08:59 +0200

    maxiterations lowered, even when a single train is done

--- Nauron/Models/Adaline.cs
+++ Nauron/Models/Adaline.cs
@@ -17,6 +17,7 @@ namespace Nauron.Models
         List<double> trainingErrors;
         Random rand;
         double learningRate;
+        long maxiterations = 100;
         public Adaline(int func)
         {
             this.func = func;
@@ -49,12 +50,12 @@ namespace Nauron.Models
         }
         public double TrainToIterations(long maxIterations)
         {
-            if (maxIterations <= 0)
+            maxiterations = maxIterations;
+            if (maxiterations <= 0)
                 throw new ArgumentException("Maksymalna liczba iteracji mniejsza lub równa 0");
 
-            while (maxIterations >= 0)
+            while (maxiterations > 0)
             {
-                maxIterations--;
                 SingleIterationTrain();
             }
             return trainedError;
@@ -65,7 +66,7 @@ namespace Nauron.Models
                 throw new ArgumentException("Nie wgrano danych");
             if (learningRate == 0.0)
                 throw new ArgumentException("Nie podano tempa uczenia");
-
+            maxiterations--;
             double sumSquaredError = 0.0;
             for (int t = 0; t < trainingX.Count; t++)
             {
